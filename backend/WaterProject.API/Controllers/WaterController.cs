57125c06f156e6ee312e94a3cfa8986084a95428
Jacob Hale <Jchale23@byui.edu>
2026-03-18 13:38:53 -0600

    initial

---
+++ backend/WaterProject.API/Controllers/WaterController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SQLitePCL;
+using WaterProject.API.Data;
+
+namespace WaterProject.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WaterController : ControllerBase
+    {
+        private WaterDbContext _context;
+        public WaterController(WaterDbContext temp)
+        {
+            _context = temp;
+        }
+
+        public IEnumerable<Project> Get()
+        {
+            var x = _context.Projects.ToList();
+            return x;
+        }
+
+
+
+    }
+}
