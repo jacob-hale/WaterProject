6547394a330e1d1be56342fbe8ba6eb533be56e1
Jacob Hale <Jchale23@byui.edu>
2026-04-03 18:38:03 -0600

    try to fix button

--- frontend/src/pages/ProjectsPage.tsx
+++ frontend/src/pages/ProjectsPage.tsx
@@ -3,15 +3,21 @@ import CategoryFilter from '../components/CategoryFilter';
 import ProjectList from '../components/ProjectList';
 import WelcomeBand from '../components/WelcomeBand';
 import CartSummary from '../components/CartSummary';
+import { useNavigate } from 'react-router-dom';
 
 function ProjectsPage() {
   const [selectedCategories, setSelectedCategories] = useState<string[]>([]);
-
+  const navigate = useNavigate();
   return (
     <div className="container mt-4">
       <CartSummary />
       <WelcomeBand />
-      <button onClick={() => (window.location.href = '/adminprojects')} className='btn btn-success'>Manage Projects</button>
+      <button
+        onClick={() => navigate('/adminprojects')}
+        className="btn btn-success"
+      >
+        Manage Projects
+      </button>{' '}
       <div className="row">
         <div className="col-md-3">
           <CategoryFilter
